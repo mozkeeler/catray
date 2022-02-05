25f54773a64ab067326064aed34ff720ce4f05f0
Dana Keeler <dkeeler@mozilla.com>
2022-02-04 21:01:00 -0800

    use browser.runtime.getURL

--- extension/background.js
+++ extension/background.js
@@ -7,7 +7,7 @@ function listener(info) {
       let id = info.url.split("/")[4];
       let catId = parseInt(id) % kTotalCats;
       let catIdStr = catId.toString().padStart(8, "0");
-      return { redirectUrl: browser.extension.getURL("cats/" + catIdStr + "-96.jpg") };
+      return { redirectUrl: browser.runtime.getURL("cats/" + catIdStr + "-96.jpg") };
     }
   } catch (e) {
     console.error(e);
